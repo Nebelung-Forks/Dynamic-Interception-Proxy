1ac8e9500afe098c15f6960c634e1b22c5e06309
EnderKingJ <78042486+EnderKingJ@users.noreply.github.com>
2022-02-27 17:19:55 +0000

    sus

--- proxify.module.js
+++ proxify.module.js
@@ -5,9 +5,10 @@ class Rewriter {
   encode(url) {
     url = url.replace(/^\/\//g, 'https://')
     var config = this.config
-    if (url.includes(location.origin+config.prefix)||url.includes(config.prefix)) return url
-    if (url.startsWith(location.origin)) url = location.origin+config.prefix+url.replace(location.origin, config.url.origin)
-    else url = config.prefix+url
+    config.op = '/v1/'
+    if (url.includes(location.origin+config.prefix)||url.includes(config.prefix)||url.startsWith(location.origin+config.op)||url.includes(config.op)) return url
+    if (url.startsWith(location.origin)) url = location.origin+config.op+url.replace(location.origin, config.url.origin)
+    else url = config.op+url
     return url
   } 
   decode(url) {
